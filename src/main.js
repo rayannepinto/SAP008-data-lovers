47e0cf8278a8c64d7261488cd00138cf078881e9
ncrigo <ncrigobr@gmail.com>
2022-08-17 18:48:02 -0300

    adicionado dados dos pokemons no card pelo JS

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
-import data from './data/pokemon/pokemon.js';
+import data from "./data/pokemon/pokemon.js";
 
-import {
-  filterByType,
-  filterByName,
-  filterByNum
-} from './data.js';
+import { filterByType, filterByName, filterByNum } from "./data.js";
 
 //* const para termos o datalist dos Pokémons *//
 const pkmnDataList = data.pokemon;
 
+//* Aparecer os pokemons no card *//
+const scrollCard = document.getElementById("scroll-card");
 
+pkmnDataList.forEach((pokemon) => {
+  const card = document.createElement("div");
+
+  card.innerHTML = `
+  <div class="card">
+  <img src=${pokemon.img} alt="foto pokemon">
+  <p>${pokemon.num}</p>
+  <p>${pokemon.name}</p>
+  <p>${pokemon.type}</p>
+  </div>
+  `;
+
+  scrollCard.appendChild(card);
+});
