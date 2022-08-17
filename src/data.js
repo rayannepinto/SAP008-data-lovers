793399a3dd518ec480df7949632ae267b4b3c2df
rayannepinto <ray.rps18@gmail.com>
2022-08-17 09:16:34 -0300

    filtro dos cards

--- src/data.js
+++ src/data.js
@@ -1,9 +1,49 @@
-// estas funciones son de ejemplo
+//* filtro por tipo *//
+const filterByType = (data, valor) => { /* definindo os parâmetros */
+  const filterPkmnByType = data.filter( /* abrindo uma const para receber o filtro */
+    (pokemon) => pokemon.type.indexOf(valor.toLowerCase()) > -1 /* buscando valor que queremos pelo indexOf */
+  );
+  return filterPkmnByType;
+}
 
-export const example = () => {
-  return 'example';
+/* filtro por nome dos Pokémons */
+const filterByName = (data, valor) => {
+  if (valor === "A-Z") {
+    data.sort((a, b) => a.name.localeCompare(b.name)); /* localeCompare compara sequências em uma localidade específica */
+  } else {
+    data.sort((a, b) => b.name.localeCompare(a.name));
+  }
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+/* filtro pelo número do Pokémon */
+const filterByNum = (data, valor) => {
+  if (valor === "0-9") {
+    data.sort((a, b) => { /* sort para ordenar os items da array */
+      if (a.num > b.num) { /* Se compare(a, b) for maior que zero, o método sort() classificará b com um índice menor que a, ou seja, b virá primeiro.*/
+        return 1;
+      } else if (a.num < b.num) { /* e compare(a, b) for menor que zero, o método sort() classifica a para um índice menor que b. Ou seja, o a virá antes de b.*/
+        return -1;
+      } else if (a.num === b.num) { /* Se compare(a, b) retornar zero, o método sort() considera a igual a b e deixa suas posições inalteradas. */
+        return 0;
+      }
+    });
+  } else {
+
+    data.sort((a, b) => {
+      if (a.num < b.num) {
+        return 1;
+      } else if (a.num > b.num) {
+        return -1
+      } else if (a.num === b.num) {
+        return 0;
+      }
+    });
+  }
+  
+};
+
+export {
+  filterByName,
+  filterByNum,
+  filterByType,
 };
