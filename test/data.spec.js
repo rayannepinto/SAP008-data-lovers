4ac52115111bf162a9987f926f119a029c0ebdf3
ncrigo <ncrigobr@gmail.com>
2022-08-26 11:32:16 -0300

    criado o teste do filterByName

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,29 @@
-import { example, anotherExample } from '../src/data.js';
+import { filterByName } from '../src/data.js';
 
+const pokemons = [
+  {
+    name: "bulbasaur"
+  },
+  {
+    name: "abra"
+  },
+  {
+    name: "zubat"
+  }
+]
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
-  });
+describe ("filterByName", function () {
+  it("deve ordernar de a-z", function () {
+    const ordenado = filterByName(pokemons, "A-Z")
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
-  });
-});
+    expect(ordenado.length).toEqual(pokemons.length)
+    expect(ordenado[0].name).toEqual(pokemons[1].name)
+  })
 
+  it("deve ordernar de z-a", function () {
+    const ordenado = filterByName(pokemons, "Z-A")
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
-
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});
+    expect(ordenado.length).toEqual(pokemons.length)
+    expect(ordenado[0].name).toEqual(pokemons[2].name)
+  })
+})
